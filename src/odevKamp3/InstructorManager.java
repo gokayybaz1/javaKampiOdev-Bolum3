28a8331cf211d0a93106c84297592dfedbc31c69
Gökay Baz <82549360+gokayybaz1@users.noreply.github.com>
2021-04-30 15:22:56 +0300

    odev...

--- src/odevKamp3/InstructorManager.java
+++ src/odevKamp3/InstructorManager.java
@@ -1,6 +1,6 @@
 package odevKamp3;
 
-public class InstructorManager {
+public class InstructorManager extends UserManager {
   public void addInstructor(InstructorUser user) {
 	  System.out.println("** "+user.getInstructorId() + " eðitmen numaralý " + user.getInstructorRole() + " " + user.getName()+" "+user.getSurName()+" sisteme eklendi !");
   }
